9c29f6f5fa6198433faeae4dbf1d262591d57596
Yash <yashshah2400@gmail.com>
2023-06-28 18:55:45 +0530

    Initial UI

---
+++ components/index.tsx
@@ -0,0 +1,17 @@
+import Hero from "./hero";
+import CustomButton from "./cutomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
+}
