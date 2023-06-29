f06fce9e88a308590216f62175169b9534a0825c
Yash <yashshah2400@gmail.com>
2023-06-30 01:49:45 +0530

    Server-side-rendering done & Fetched Image API

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { SearchBar, CustomFilter, Hero, CarCard } from '@/components'
 import Image from 'next/image'
 import { fetchCars } from "@/utils";
+import { HomeProps } from '@/types';
 
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({searchParams}: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
